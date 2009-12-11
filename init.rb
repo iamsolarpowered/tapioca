aa8a087466dcab49e9a06c8fa3607ab6a31e8ec6
Ben Shymkiw <ben@tapiocacollective.com>
2009-12-11 14:47:13 -0500

    fixed reloading error in development

--- init.rb
+++ init.rb
@@ -1,1 +1,8 @@
 ActionView::Base.send :include, TapiocaHelper
+
+# dirty fix to make engine models load properly in development
+if ENV['RAILS_ENV'] != 'production'
+  load_paths.each do |path|
+    ActiveSupport::Dependencies.load_once_paths.delete(path)
+  end
+end
